09dc1d23b469139f15e37aa6b2409dbca436d2e1
Steve Wakham <juzzy@juzzy.com>
2025-07-10 00:20:39 -0500

    Update Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,9 +29,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
+        'username'     => 'bloomingwire',
+        'password'     => 'bloomingwire.pw',
+        'database'     => 'bloomingwire',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
